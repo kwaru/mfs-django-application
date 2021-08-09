26f43c63c4549788269426af638581a56be99522
Maurice Omondi Okoth <momondi547@gmail.com>
2021-08-09 11:39:05 +0300

    updated settings.py

--- CoordinatesApp/settings.py
+++ CoordinatesApp/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ozx-o!w7p&ct)%uad0k%c=8cuqo&qeewd2+bg0@s4#rpz2kt89'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
